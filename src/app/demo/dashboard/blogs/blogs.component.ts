a1e1e98a99d5f8e2d6b6fe6e53b10a05be41f04b
Chris <chris@attainanalytics.com>
2022-11-19 00:12:25 -0600

    Re-designed the home (default) page - work still in progress

--- src/app/demo/dashboard/blogs/blogs.component.ts
+++ src/app/demo/dashboard/blogs/blogs.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import SampleJSON from 'src/fake-data/trend-data.json';
+import { random } from 'lodash-es';
+
+declare var $: any;
 
 @Component({
   selector: 'app-blogs',
@@ -7,6 +11,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BlogsComponent implements OnInit {
   constructor() {}
+  topics: any[] = SampleJSON;
+  blogImageId0 = random(0, this.topics[0].images.length - 1);
+  blogImageId1 = random(0, this.topics[1].images.length - 1);
+  blogImageId2 = random(0, this.topics[2].images.length - 1);
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.blogImageId0 = random(0, this.topics[0].images.length - 1);
+    this.blogImageId1 = random(0, this.topics[1].images.length - 1);
+    this.blogImageId2 = random(0, this.topics[2].images.length - 1);
+  }
 }
