90723b2e195c70abb673d289f5298ba1de08508c
Chris <chris@attainanalytics.com>
2022-11-19 22:10:42 -0600

    Started the implementation of the search result page initiated from the default/home page

--- src/app/demo/dashboard/default/default.component.ts
+++ src/app/demo/dashboard/default/default.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import SampleJSON from 'src/fake-data/trend-data.json';
+import { random } from 'lodash-es';
 
 @Component({
   selector: 'app-default',
@@ -7,10 +9,21 @@ import SampleJSON from 'src/fake-data/trend-data.json';
   styleUrls: ['./default.component.scss']
 })
 export class DefaultComponent implements OnInit {
-  constructor() {}
+  searchBox = '';
+  constructor(private router: Router) {}
   topics: any[] = SampleJSON;
+  blogImageId0 = random(0, this.topics[0].images.length - 1);
+  blogImageId1 = random(0, this.topics[1].images.length - 1);
+  blogImageId2 = random(0, this.topics[2].images.length - 1);
 
   ngOnInit() {
-    console.log(this.topics);
+    this.blogImageId0 = random(0, this.topics[0].images.length - 1);
+    this.blogImageId1 = random(0, this.topics[1].images.length - 1);
+    this.blogImageId2 = random(0, this.topics[2].images.length - 1);
+  }
+
+  searchQuery(searchText: string): void {
+    console.log(searchText);
+    this.router.navigateByUrl('/dashboard/default/search-result?searchText');
   }
 }
