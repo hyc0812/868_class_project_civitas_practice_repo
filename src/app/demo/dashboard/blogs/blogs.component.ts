2966ad532bd7770c826cacdc06e6962f7292b0ce
yongchang he <hyc0812@163.com>
2022-11-30 16:52:34 -0600

    modified content for blogs page

--- src/app/demo/dashboard/blogs/blogs.component.ts
+++ src/app/demo/dashboard/blogs/blogs.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import SampleJSON from 'src/fake-data/trend-data.json';
+import SampleJSON from 'src/fake-data/search-result-aoa-data.json';
 import { random } from 'lodash-es';
 
-declare var $: any;
 
 @Component({
   selector: 'app-blogs',
@@ -11,14 +10,51 @@ declare var $: any;
 })
 export class BlogsComponent implements OnInit {
   constructor() {}
+  dtRowSelectOptions: any = {};
   topics: any[] = SampleJSON;
-  blogImageId0 = random(0, this.topics[0].images.length - 1);
-  blogImageId1 = random(0, this.topics[1].images.length - 1);
-  blogImageId2 = random(0, this.topics[2].images.length - 1);
+  
 
   ngOnInit(): void {
-    this.blogImageId0 = random(0, this.topics[0].images.length - 1);
-    this.blogImageId1 = random(0, this.topics[1].images.length - 1);
-    this.blogImageId2 = random(0, this.topics[2].images.length - 1);
+
+    //Display table data
+    this.displayTable(this.topics);
+  }
+    
+  
+
+  displayTable(returnedData: any[]) {
+    this.dtRowSelectOptions = {
+      data: returnedData, //'fake-data/search-result-data.json',
+      columns: [
+        {
+          title: 'Title'
+          //data: 'title'
+        },
+        {
+          title: 'Post Type'
+          //data: 'postType'
+        },
+        {
+          title: 'Posted On'
+          //data: 'postedOn'
+        },
+        {
+          title: 'Author'
+          //data: 'author'
+        },
+        {
+          title: 'Action',
+          
+            "data": null,
+            //"defaultContent": '<button class="btn btn-outline-primary btn-sm">View</button>',
+            //"defaultContent":'<a [routerLink]="[/dashboard/blogs/show-blog, memberPost.id]"  type="button"  (click)="onSelect(memberPost)" >View </a>',
+            "defaultContent":'<a href="/dashboard/blogs/show-blog/1"  type="button" >View </a>',
+            "targets": -1
+          
+        }
+      ],
+      responsive: true,
+      select: true
+    };
   }
 }
--- src/app/demo/dashboard/blogs/blogs.module.ts
+++ src/app/demo/dashboard/blogs/blogs.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { BlogsComponent } from './blogs.component';
 import { SharedModule } from '../../../theme/shared/shared.module';
 import { BlogsRoutingModule } from './blogs-routing.module';
+import { DataTablesModule } from 'angular-datatables';
 
 @NgModule({
   declarations: [BlogsComponent],
-  imports: [CommonModule, SharedModule, BlogsRoutingModule]
+  imports: [CommonModule, SharedModule, BlogsRoutingModule, DataTablesModule]
 })
 export class BlogsModule {}
