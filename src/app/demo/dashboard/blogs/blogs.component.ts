09425ad3f439527e8b45921c060863581d4c27f9
Yongchang He <80454212+hyc0812@users.noreply.github.com>
2022-11-30 23:26:09 -0600

    defaultContent in trouble

--- src/app/demo/dashboard/blogs/blogs.component.ts
+++ src/app/demo/dashboard/blogs/blogs.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import SampleJSON from 'src/fake-data/search-result-aoa-data.json';
 import MembersDB from '../../../../fake-data/members.json';
+import { Members } from '../members';
 
 
 @Component({
@@ -16,14 +17,12 @@ export class BlogsComponent implements OnInit {
   dtRowSelectOptions: any = {};
   topics: any[] = MembersDB;
   
-
   ngOnInit(): void {
-
     //Display table data
     this.displayTable(this.topics);
+    // console.log(this.topics[1].id);
   }
     
-  
 
   displayTable(returnedData: any[]) {
     this.dtRowSelectOptions = {
@@ -47,10 +46,11 @@ export class BlogsComponent implements OnInit {
             "data": null,
             //"defaultContent": '<button class="btn btn-outline-primary btn-sm">View</button>',
             //"defaultContent":'<a [routerLink]="[/dashboard/blogs/show-blog, memberPost.id]"  type="button"  (click)="onSelect(memberPost)" >View </a>',
-            "defaultContent":'<a href="/dashboard/blogs/show-blog/:id"  type="button" >View </a>',
+            //"defaultContent":'<a routerLink="/dashboard/blogs/show-blog/{{ memberPost.postId }}" (click)="onSelect(memberPost)" type="button" >View </a>',
+            "defaultContent":`<a href=/dashboard/blogs/show-blog/${this.topics[1].id} type="button" >View </a>`,
             "targets": -1
           
-        }
+        }      
       ],
       responsive: true,
       select: true
