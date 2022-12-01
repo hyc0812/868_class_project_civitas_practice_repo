e1c87c9dffe5b8bbc51d5009aa7809a8d0bd9f36
Yongchang He <80454212+hyc0812@users.noreply.github.com>
2022-12-01 15:30:08 -0600

    initialize this branch with default code example

--- src/app/demo/dashboard/create-post/create-post.component.ts
+++ src/app/demo/dashboard/create-post/create-post.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Hero } from './hero';
+
 
 @Component({
   selector: 'app-create-post',
@@ -6,6 +8,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-post.component.scss']
 })
 export class CreatePostComponent implements OnInit {
+
+  powers = ['Really Smart', 'Super Flexible',
+  'Super Hot', 'Weather Changer'];
+
+  model = new Hero(18, 'Dr. IQ', this.powers[0], 'Chuck Overstreet');
+
+  submitted = false;
+
+  onSubmit() { this.submitted = true; }
+
+
+
+  newHero() {
+    this.model = new Hero(42, '', '');
+  }
+
+  skyDog(): Hero {
+    const myHero =  new Hero(42, 'SkyDog',
+                           'Fetch any object at any distance',
+                           'Leslie Rollover');
+    console.log('My hero is called ' + myHero.name); // "My hero is called SkyDog"
+    return myHero;
+  }
+
+  //////// NOT SHOWN IN DOCS ////////
+
+  // Reveal in html:
+  //   Name via form.controls = {{showFormControls(heroForm)}}
+  showFormControls(form: any) {
+    return form && form.controls.name &&
+    form.controls.name.value; // Dr. IQ
+  }
+
+  /////////////////////////////
+
+
 
   constructor() { }
 
