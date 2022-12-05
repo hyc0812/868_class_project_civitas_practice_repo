c7dbc211cb11a8a775a3b78a3da8fb58e4341d8c
Yongchang He <80454212+hyc0812@users.noreply.github.com>
2022-12-04 19:06:33 -0600

    added feathers to 'help' page

--- src/app/demo/dashboard/help/help.module.ts
+++ src/app/demo/dashboard/help/help.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { HelpComponent } from './help.component';
 import { SharedModule } from '../../../theme/shared/shared.module';
 import { HelpRoutingModule } from './help-routing.module';
+import { ArchwizardModule } from 'angular-archwizard';
 
 @NgModule({
   declarations: [HelpComponent],
-  imports: [CommonModule, SharedModule, HelpRoutingModule]
+  imports: [CommonModule, SharedModule, HelpRoutingModule, ArchwizardModule]
 })
 export class HelpModule {}
